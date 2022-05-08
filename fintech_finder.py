5ac93757865c9ae78f637ff0f16c2c6cbb9d29d2
Erin Rahman <93865608+e-rahman@users.noreply.github.com>
2022-05-07 19:35:32 -0500

    fintech finder

---
+++ fintech_finder.py
@@ -0,0 +1,220 @@
+# Cryptocurrency Wallet
+
+################################################################################
+
+# Imports
+
+import os
+from dotenv import load_dotenv
+from bip44 import Wallet
+from mnemonic import Mnemonic
+import streamlit as st
+from dataclasses import dataclass
+from typing import Any, List
+from web3 import Web3, Account
+from web3 import EthereumTesterProvider
+from eth_tester import PyEVMBackend, EthereumTester
+from ethereum import w3, generate_account, get_balance, send_transaction
+w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+
+################################################################################
+
+# From `crypto_wallet.py import the functions generate_account, get_balance, and send_transaction
+
+def generate_account():
+    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+
+    # Fetch mnemonic from environment variable.
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet Object
+    wallet = Wallet(mnemonic)
+
+    # Derive Ethereum Private Key
+    private, public = wallet.derive_account("eth")
+
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    return account
+
+
+def get_balance(w3, address):
+    """Using an Ethereum account address access the balance of Ether"""
+
+    # Get balance of address in Wei
+    wei_balance = w3.eth.get_balance(address)
+
+    # Convert Wei value to ether
+    ether = w3.fromWei(wei_balance, "ether")
+
+    # Return the value in ether
+    return ether
+
+
+def send_transaction(w3, account, to, wage):
+    """Send an authorized transaction to the Ganache blockchain."""
+
+    # Set gas price strategy
+    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    value = w3.toWei(wage, "ether")
+
+    # Calculate gas estimate
+    gasEstimate = w3.eth.estimateGas({"to": to, "from": account.address, "value": value})
+
+    # Construct a raw transaction
+    raw_tx = {
+        "to": to,
+        "from": account.address,
+        "value": value,
+        "gas": gasEstimate,
+        "gasPrice": 0,
+        "nonce": w3.eth.getTransactionCount(account.address)
+    }
+
+    # Sign the raw transaction with ethereum account
+    signed_tx = account.signTransaction(raw_tx)
+
+    # Send the signed transactions
+    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+################################################################################
+
+# Fintech Finder Candidate Information
+
+# Database of Fintech Finder candidates including their name, digital address, rating and hourly cost per Ether.
+# A single Ether is currently valued at $1,500
+candidate_database = {
+    "Lane": ["Lane", "0xaC8eB8B2ed5C4a0fC41a84Ee4950F417f67029F0", "4.3", .20, "Images/lane.jpeg"],
+    "Ash": ["Ash", "0x2422858F9C4480c2724A309D58Ffd7Ac8bF65396", "5.0", .33, "Images/ash.jpeg"],
+    "Jo": ["Jo", "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45", "4.7", .19, "Images/jo.jpeg"],
+    "Kendall": ["Kendall", "0x8fD00f170FDf3772C5ebdCD90bF257316c69BA45", "4.1", .16, "Images/kendall.jpeg"]
+}
+
+# A list of the FinTech Finder candidates first names
+people = ["Lane", "Ash", "Jo", "Kendall"]
+
+
+def get_people():
+    """Display the database of Fintech Finders candidate information."""
+    db_list = list(candidate_database.values())
+
+    for number in range(len(people)):
+        st.image(db_list[number][4], width=200)
+        st.write("Name: ", db_list[number][0])
+        st.write("Ethereum Account Address: ", db_list[number][1])
+        st.write("FinTech Finder Rating: ", db_list[number][2])
+        st.write("Hourly Rate per Ether: ", db_list[number][3], "eth")
+        st.text(" \n")
+
+################################################################################
+
+# Streamlit Code
+
+# Streamlit application headings
+st.markdown("# Fintech Finder!")
+st.markdown("## Hire A Fintech Professional!")
+st.text(" \n")
+
+################################################################################
+
+# Streamlit Sidebar Code - Start
+st.sidebar.markdown("## Client Account Address and Ethernet Balance in Ether")
+
+################################################################################
+
+#  Call the `generate_account` function and save it as the variable `account`
+account = generate_account(w3)
+
+################################################################################
+
+# Write the client's Ethereum account address to the sidebar
+st.text("\n")
+st.text("\n")
+st.markdown("## Ethereum Account Address:")
+
+# Write the Ethereum account address to the Streamlit page
+st.write(account.address)
+
+################################################################################
+
+# Call `get_balance` function and pass it your account address
+# Write the returned ether balance to the sidebar
+
+# Display the Etheremum Account balance
+st.text("\n")
+st.text("\n")
+st.markdown("## Ethereum Account Balance:")
+
+# Call the get_balance function and write the account balance to the screen
+ether_balance = get_balance(w3, account.address)
+st.write(ether_balance)
+
+################################################################################
+
+# Create a select box to choose a FinTech Hire candidate
+person = st.sidebar.selectbox('Select a Person', people)
+
+# Create an input field to record the number of hours the candidate worked
+hours = st.sidebar.number_input("Number of Hours")
+
+st.sidebar.markdown("## Candidate Name, Hourly Rate, and Ethereum Address")
+
+# Identify the FinTech Hire candidate
+candidate = candidate_database[person][0]
+
+# Write the Fintech Finder candidate's name to the sidebar
+st.sidebar.write(candidate)
+
+# Identify the FinTech Finder candidate's hourly rate
+hourly_rate = candidate_database[person][3]
+
+# Write the inTech Finder candidate's hourly rate to the sidebar
+st.sidebar.write(hourly_rate)
+
+# Identify the FinTech Finder candidate's Ethereum Address
+candidate_address = candidate_database[person][1]
+
+# Write the inTech Finder candidate's Ethereum Address to the sidebar
+st.sidebar.write(candidate_address)
+
+# Write the Fintech Finder candidate's name to the sidebar
+st.sidebar.markdown("## Total Wage in Ether")
+
+################################################################################
+
+# Sign and Execute a Payment Transaction
+
+# Calculate candidate's wage
+wage = candidate_database[person][3] * hours
+
+# Write the candidate's wage to the sidebar
+st.sidebar.write("If you hire", candidate, "for", hours, "the candidate's wage will be", wage, ".")
+
+################################################################################
+
+# Save the transaction hash that the `send_transaction` function returns as a
+# variable named `transaction_hash`, and have it display on the application’s
+# web interface.
+
+if st.sidebar.button("Send Transaction"):
+
+    # Call the `send_transaction` function and pass it 3 parameters:
+    # Your `account`, the `candidate_address`, and the `wage` as parameters
+    # Save the returned transaction hash as a variable named `transaction_hash`
+    transaction_hash = send_transaction(account, candidate_address, wage)
+
+    # Markdown for the transaction hash
+    st.sidebar.markdown("#### Validated Transaction Hash")
+
+    # Write the returned transaction hash to the screen
+    st.sidebar.write(transaction_hash)
+
+    # Celebrate your successful payment
+    st.balloons()
+
+# The function that starts the Streamlit application
+# Writes FinTech Finder candidates to the Streamlit page
+get_people()
